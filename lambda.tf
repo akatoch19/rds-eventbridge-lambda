e2624669e37e830f0261489c35c1d8915bbe3d7e
unknown <akatoch19@gmail.com>
2025-06-17 18:15:51 +0530

    lambda -rds - event bridge

--- lambda.tf
+++ lambda.tf
@@ -1,3 +1,22 @@
+resource "aws_security_group" "lambda_sg" {
+  name   = "lambda-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    security_groups = [var.rds_sg_id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_lambda_function" "rds_trigger_lambda" {
   filename         = "lambda.zip"
   function_name    = "rds_trigger_lambda"
@@ -15,6 +34,12 @@ resource "aws_lambda_function" "rds_trigger_lambda" {
       DB_PORT = var.db_port
 
     }
+  }
+
+
+  vpc_config {
+    subnet_ids         = var.private_subnet_ids
+    security_group_ids = [aws_security_group.lambda_sg.id]
   }
 
   timeout = 10
--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
 variables "db_host" {}
 variables "db_name" {}
 variables "db_port" {}
+variable "vpc_id" {}
+variable "private_subnet_ids" {
+  type = list(string)
+}
+variable "rds_sg_id" {}
