dbbb9e9f70c7f90fd95be11b5b651e2985183a41
unknown <akatoch19@gmail.com>
2025-06-18 20:48:04 +0530

    Added files

--- variables.tf
+++ variables.tf
@@ -4,7 +4,10 @@ variable "vpc_id" {}
 variable "private_subnet_ids" {
   type = list(string)
 }
-variable "rds_sg_id" {}
+variable "rds_sg_id" {
+  type        = string
+  default = "sg-062124e2fbb469f15"
+}
 variable "db_secret_arn" {
   description = "The ARN of the DB secret"
   type        = string
